f425c36d10f51c78ed2abb202d5fb6cf3165c5ac
Nicolas Candelara <61365538+NicolasCandelaria@users.noreply.github.com>
2021-11-18 20:57:06 -0800

    Added sound fx

--- scripts.js
+++ scripts.js
@@ -15,6 +15,17 @@ let SELECTED_PIECE = null;
 let START_TIME = null;
 let END_TIME = null;
 
+let POP_SOUND = new Audio('popsound.mp3');
+POP_SOUND.volume = 0.1;
+
+let AUDIO_CONTEXT = new (AudioContext || webkitAudioContext || window.webkitAudioContext)();
+
+let keys = {
+    DO:261.6,
+    RE:293.7,
+    MI: 329.6
+}
+
 function main() {
     CANVAS = document.getElementById("myCanvas");
     CONTEXT = CANVAS.getContext("2d");
@@ -187,6 +198,7 @@ function onMouseUp() {
         if (isComplete() && END_TIME == null) {
             let now = new Date().getTime();
             END_TIME = now;
+            setTimeout(playMelody(), 500);
         }
     }
     SELECTED_PIECE = null;
@@ -316,6 +328,8 @@ class Piece {
         this.x = this.xCorrect;
         this.y = this.yCorrect;
         this.correct = true;
+        //plays the pop sound when a piece snaps in
+        POP_SOUND.play();
     }
 }
 /*------------------------------
@@ -325,4 +339,41 @@ function distance(p1, p2) {
     return Math.sqrt(
         (p1.x - p2.x) * (p1.x - p2.x) +
         (p1.y - p2.y) * (p1.y - p2.y));
+}
+/*------------------------------
+    This plays a single note that has a specified key and a duration
+    ------------------------------*/
+function playNote(key, duration) {
+    let osc=AUDIO_CONTEXT.createOscillator();
+    osc.frequency.value=key;
+    osc.start(AUDIO_CONTEXT.currentTime);
+    osc.stop(AUDIO_CONTEXT.currentTime+duration/1000);
+
+let envelope = AUDIO_CONTEXT.createGain();
+osc.connect(envelope);
+osc.type='triangle';
+envelope.connect(AUDIO_CONTEXT.destination);
+envelope.gain.setValueAtTime(0, AUDIO_CONTEXT.currentTime);
+envelope.gain.linearRampToValueAtTime(0.5, AUDIO_CONTEXT.currentTime+0.1);
+envelope.gain.linearRampToValueAtTime(0, AUDIO_CONTEXT.currentTime+duration/1000);
+
+    setTimeout(function() {
+        osc.disconnect();
+    }, duration);
+}
+
+function playMelody() {
+    playNote(keys.DO, 300);
+    setTimeout(function(){
+        playNote(keys.RE, 300);
+    }, 700);
+    setTimeout(function(){
+        playNote(keys.MI, 300);
+    }, 400);
+    setTimeout(function(){
+        playNote(keys.RE, 300);
+    }, 600);
+    setTimeout(function(){
+        playNote(keys.DO, 300);
+    }, 900);
 }
